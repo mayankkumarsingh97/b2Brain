fe64e6f61d7d46156bc5e6a0cb1664e18834709c
Mayank kumar singh <mayank.reactjs@gmail.com>
2022-07-16 08:47:25 +0530

    refactored

--- src/index.js
+++ src/index.js
@@ -3,34 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-// import Contact from './components/contactus/Contact';
-// import Body  from "./components/body/Body";
-// import Myform02 from "./components/registration/Myform02";
-// import { BrowserRouter } from "react-router-dom";
-// import { Routes, Route} from "react-router-dom";
-// import PageNotF from './materialUI/components/pagenotfound/PageNotF';
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App/>
-
-  // <BrowserRouter>
-  
-
-  //       <Routes>
-  //         <Route path='/' element={<App/>}> 
-  //          <Route path='/Contact' element={<Contact/>} />
-  //          <Route path='/registration' element={<Myform02/>} />
-  //          <Route index  element={<Body/>} />
-  //          <Route path='*' element={<PageNotF/>}/>
-  //         </Route>
-            
-  //       </Routes>
-  //     </BrowserRouter>
-       
 );
 
 
